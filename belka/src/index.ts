9a7366598611901edd2cbaed46d401f7075bcb45
David Futcher <david@futcher.io>
2020-07-25 19:26:35 +0100

    Tweak Belka compiler flags, implement example countdown actor

--- belka/src/index.ts
+++ belka/src/index.ts
@@ -1,15 +1,21 @@
-// The entry file of your WebAssembly module.
-
-// Import our host-exposed `log` function
 import { log } from './host';
 import { register } from './stream';
 
+var countdownVal = 10;
+
 function on_timer_tick(): void {
-  log('in on_timer_tick');
+  if (countdownVal == 10) {
+    log('Countdown initiated');
+  } else if (countdownVal == 0) {
+    log('Ignition start');
+    abort();
+  }
+  
+  log(countdownVal.toString());
+  countdownVal -= 1;
 }
 
 export function init(): u8 {
-  log("Hello, World!");
   register('timer:seconds', on_timer_tick);
   return 0;
 }
